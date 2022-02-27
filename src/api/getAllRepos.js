2a90656d2bbc402c8da9ca0e7fdca89430cade3e
Haroon Ahmad <haroon334@gmail.com>
2022-02-28 00:17:17 +0100

    Fix: updated get requests to avoid .env/auth errors

--- src/api/getAllRepos.js
+++ src/api/getAllRepos.js
@@ -3,7 +3,7 @@ import httpService, { headers } from "../services/http.js";
 export const getAllRepos = (query) => {
   return httpService.get(
     `https://api.github.com/search/repositories?q=${query}&type=repositories&per_page=100`,
-    {
+    headers.Authorization && {
       headers: headers,
     }
   );
--- src/api/getUserRepos.js
+++ src/api/getUserRepos.js
@@ -3,7 +3,7 @@ import httpService, { headers } from "../services/http.js";
 export const getUserRepo = (user) => {
   return httpService.get(
     `https://api.github.com/users/${user}/repos?per_page=150`,
-    {
+    headers.Authorization && {
       headers: headers,
     }
   );
